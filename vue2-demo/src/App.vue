5a955ed36730c516a2401ff69df73d503b4f7f7c
huanghaoming <1585685035@qq.com>
2023-09-18 21:24:26 +0800

    整合了photoswipe

--- vue2-demo/src/App.vue
+++ vue2-demo/src/App.vue
@@ -6,14 +6,62 @@
     <br>
     <router-link to="/Hi">hi</router-link>
     <hr />
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <div id="my-test-gallery">
+      <a v-for="(image, key) in images" :key="key" :href="image.largeURL" :data-pswp-width="image.width"
+        :data-pswp-height="image.height" target="_blank" rel="noreferrer">
+        <img :src="image.thumbnailURL" alt="" />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      images: [
+        {
+          largeURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg',
+          thumbnailURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-200.jpg',
+          width: 1875,
+          height: 2500,
+        },
+        {
+          largeURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg',
+          thumbnailURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg',
+          width: 1669,
+          height: 2500,
+        },
+        {
+          largeURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg',
+          thumbnailURL:
+            'https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg',
+          width: 2500,
+          height: 1666,
+        },
+      ],
+    }
+  },
+  mounted() {
+    if (!this.lightbox) {
+      this.lightbox = new PhotoSwipeLightbox({
+        gallery: '#my-test-gallery',
+        children: 'a',
+        pswpModule: () => import('photoswipe'),
+      });
+      this.lightbox.init();
+    }
+  },
   components: {
   }
 }
